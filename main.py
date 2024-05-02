13b87798c4d300616221202b699a439ee32cf847
Hendrik <dingerkus.hendrik@proton.me>
2024-05-02 13:44:43 +0200

    fahrenheit und logs

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #! /bin/python3
-# echo "bme280 0x76" > /sys/bus/i2c/devices/i2c-1/new_device
 import time
-from datetime import datetime
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 
@@ -12,6 +11,16 @@ def read_file(filename):
     with open(filename, "r") as f:
         data = f.read().strip()
     return data
+
+
+def log(filename, timestamp, temp_c, temp_f, humidity, pressure):
+    with open(filename, "a") as f:
+        f.write(str(timestamp) + "\n")
+        f.write(str(temp_c) + "\n")
+        f.write(str(temp_f) + "\n")
+        f.write(str(humidity) + "\n")
+        f.write(str(pressure) + "\n")
+    f.close()
 
 
 def fan_on():
@@ -24,27 +33,32 @@ def fan_off():
 
 def main():
     while True:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time)
+        now = time.localtime()
+        current_time = time.strftime("========== %d.%M.%y %H:%M:%S ==========", now)
 
         path = "/sys/bus/iio/devices/iio:device0/"
         temperature = read_file(f'{path}in_temp_input')
         temperature = float(temperature) / 1000.0
         temperature = round(temperature, 2)
 
+        fahrenheit = temperature * 9.0 / 5.0 + 32.0
+        fahrenheit = round(fahrenheit, 2)
+
         humidity = read_file(f'{path}in_humidityrelative_input')
-        humidity = float(humidity) / 1000.0
-        humidity = round(humidity, 2)
+        humidity = int(float(humidity)) / 1000
+        humidity = int(humidity)
 
         pressure = read_file(f'{path}in_pressure_input')
-        pressure = float(pressure) * 10
-        pressure = round(pressure, 2)
+        pressure = int(float(pressure)) * 10
 
-        print(str(temperature) + "°C")
-        print(str(humidity) + "%")
-        print(str(pressure) + "hPa")
+        print(current_time)
+        print("Temperatur (Celsius): " + str(temperature) + "°C")
+        print("Temperatur (Fahrenheit): " + str(fahrenheit) + "°F")
+        print("Luftfeuchte: " + str(humidity) + "%")
+        print("Luftdruck: " + str(pressure) + "hPa")
         print()
+
+        log("/opt/lueftersteuerung/log.txt", current_time, temperature, fahrenheit, humidity, pressure)
 
         if temperature > 30.0:
             fan_on()
