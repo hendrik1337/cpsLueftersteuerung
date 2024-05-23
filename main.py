522aa9d4168b4ffce49e5373657f6275dbf04e0f
Hendrik <dingerkus.hendrik@proton.me>
2024-05-23 13:20:28 +0200

    send data to influx

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import math
 import time
 import os
+from influxdb_client import InfluxDBClient, Point, WritePrecision
 
 
 def read_file(filename):
@@ -19,6 +20,34 @@ def log(filename, timestamp, temp_c, temp_f, humidity, pressure, taupunkt):
         f.write("Luftdruck: " + str(pressure) + "hPa\n")
         f.write("Taupunkt: " + str(taupunkt) + "°C\n")
     f.close()
+
+
+def send_to_influx(timestamp, temp_c, temp_f, humidity, pressure, taupunkt):
+    print(timestamp)
+    url = "http://localhost:8086"  # Replace with your InfluxDB URL
+    token = "LTEybj-7h-ko_whHAeyTFEJp4b3alfBZbS_2lAScNSFA4Pe5S0ECmg-BsntnzfQZo0-z9uTF5yOv85kTC8Yk_g=="  # Replace with your InfluxDB token
+    org = "gruppe2"
+    with InfluxDBClient(url=url, token=token, org=org) as client:
+        with client.write_api() as write_api:
+            try:
+                # Create a Point object with your data
+                point = Point("bme280") \
+                    .tag("device", "sensor1") \
+                    .field("temp_c", temp_c) \
+                    .field("temp_f", temp_f) \
+                    .field("humidity", humidity) \
+                    .field("pressure", pressure) \
+                    .field("taupunkt", taupunkt) \
+                    .time(int(time.time() * 1000), WritePrecision.MS)
+
+                # Write the point to InfluxDB
+                write_api.write(
+                    bucket="gruppe2",
+                    org="gruppe2",
+                    record=point
+                )
+            except Exception as e:
+                print(e)
 
 
 def taupunkt_berechnen(temperatur, luftfeuchtigkeit):
@@ -53,7 +82,7 @@ def fan_off():
 def main():
     while True:
         now = time.localtime()
-        current_time = time.strftime("========== %d.%M.%y %H:%M:%S ==========", now)
+        current_time = time.strftime("========== %d.%m.%y %H:%M:%S ==========", now)
 
         path = "/sys/bus/iio/devices/iio:device0/"
         temperature = read_file(f'{path}in_temp_input')
@@ -82,8 +111,10 @@ def main():
         print()
 
         log("/opt/lueftersteuerung/log.txt", current_time, temperature, fahrenheit, humidity, pressure, taupunkt)
-        csv_export("/opt/lueftersteuerung/auswertung/influx_data/log.csv", time.strftime("%d.%M.%y %H:%M:%S", now), temperature, fahrenheit,
+        csv_export("/opt/lueftersteuerung/auswertung/influx_data/log.csv", time.strftime("%d.%m.%y %H:%M:%S", now),
+                   temperature, fahrenheit,
                    humidity, pressure, taupunkt)
+        send_to_influx(time.strftime("%d.%m.%y %H:%M:%S", now), temperature, fahrenheit, humidity, pressure, taupunkt)
 
         if temperature > 30.0:
             fan_on()
